8b55a463e5f39b5a59908e09515bae2a0ca54f0f
Edmir-Demaj <edmir_demaj@yahoo.com>
2023-01-01 23:51:15 +0000

    Add code to calculate two random numbers for the game question

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,34 @@
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function () {
+    // get all the buttons on the html page and return them as an array
+    let buttons = document.getElementsByTagName("button");
+
+    // iterate over the array by selecting each item without need of index
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        })
+    }
+})
+
+/**
+ * The main game "loop", called when the script is loaded first
+ * and after the user's answer has been process
+ */
 function runGame() {
 
+    // create two random numbers between 1 and 25
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
 }
+
 
 function checkAnswer() {
 
