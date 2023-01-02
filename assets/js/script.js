7e5473f978fb26c99fa3fae2d7c34d6fd880fee9
Edmir-Demaj <edmir_demaj@yahoo.com>
2023-01-02 00:38:09 +0000

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,21 +12,34 @@ document.addEventListener("DOMContentLoaded", function () {
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
+                // call the function runGame
             }
         })
     }
+
+    runGame("addition");
 })
 
 /**
  * The main game "loop", called when the script is loaded first
  * and after the user's answer has been process
  */
-function runGame() {
+function runGame(gameType) {
 
     // create two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        // num1,2 are parameters of the function below
+        // and here we call this function giving 
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}.Aborting!`
+    }
+
 }
 
 
@@ -45,9 +58,11 @@ function incrementScore() {
 function incrementWrongAnswer() {
 
 }
-
-function displayAdditionQuestion() {
-
+// two arguments are the operands1,2
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 }
 
 function displaySubtractQuestion() {
